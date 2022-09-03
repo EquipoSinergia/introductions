eec40d434b13a6f29932705aebf9d57fb58eef7c
jeapenach <jeapenach@gmail.com>
2022-09-03 15:16:40 -0500

    Se crea servicio para buscar empresa por ID

--- Sinergia/src/main/java/proyecto/sinergia/controller/EmpresaController.java
+++ Sinergia/src/main/java/proyecto/sinergia/controller/EmpresaController.java
@@ -1,18 +1,18 @@
 package proyecto.sinergia.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import proyecto.sinergia.entities.Empresa;
+import proyecto.sinergia.repositories.EmpresaRepository;
 import proyecto.sinergia.services.EmpresaService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmpresaController {
 
-    EmpresaService empresaService;
+    private EmpresaService empresaService;
 
     public EmpresaController(EmpresaService empresaService){
         this.empresaService = empresaService;
@@ -26,6 +26,12 @@ public class EmpresaController {
     @PostMapping("/empresas")
     public Empresa createEmpresa(@RequestBody Empresa empresa){
         return this.empresaService.createEmpresa(empresa);
+    }
+
+    //Metodo para listar Empresas por su Id
+    @GetMapping("/empresas/{id}")
+    public Optional<Empresa> getEmpresaById(@PathVariable("id") long id){
+        return this.empresaService.getEmpresaById(id);
     }
 
 
--- Sinergia/src/main/java/proyecto/sinergia/services/EmpresaService.java
+++ Sinergia/src/main/java/proyecto/sinergia/services/EmpresaService.java
@@ -1,10 +1,12 @@
 package proyecto.sinergia.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import proyecto.sinergia.entities.Empresa;
 import proyecto.sinergia.repositories.EmpresaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpresaService {
@@ -21,4 +23,13 @@ public class EmpresaService {
     public Empresa createEmpresa(Empresa newEmpresa){
         return this.empresaRepository.save(newEmpresa);
     }
+
+    public Optional<Empresa> getEmpresaById(@PathVariable("id") Long empresaId){
+        java.util.Optional<Empresa> optionalEmpresa = empresaRepository.findById(empresaId);
+        if(((java.util.Optional<Empresa>) optionalEmpresa).isPresent()) {
+            return Optional.of(optionalEmpresa.get());
+        }else
+            return Optional.empty();
+    }
+
 }
