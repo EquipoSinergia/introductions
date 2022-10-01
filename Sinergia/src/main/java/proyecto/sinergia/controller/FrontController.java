a4883818977f11aa7c134ec8cd683ded51ce5ec5
jeapenach <jeapenach@gmail.com>
2022-10-01 14:41:59 -0500

    arreglo css y configuración oauth0

---
+++ Sinergia/src/main/java/proyecto/sinergia/controller/FrontController.java
@@ -0,0 +1,20 @@
+package proyecto.sinergia.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontController {
+    @GetMapping
+    public String index(Model model, @AuthenticationPrincipal OidcUser principal){
+        return "index";
+    }
+
+    //@GetMapping("/main")
+    //public String main(){
+    //    return "index";
+    //}
+}
